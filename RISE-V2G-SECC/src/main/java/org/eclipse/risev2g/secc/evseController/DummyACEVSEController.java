bf406353f8d45ce8bddf27870a1fc31b2a1250ce
Marc Mültin <marc.mueltin@chargepartner.com>
2016-04-15 14:03:23 +0200

    slight modification of copyright header
    Signed-off-by: Marc Mültin <marc.mueltin@chargepartner.com>

--- RISE-V2G-SECC/src/main/java/org/eclipse/risev2g/secc/evseController/DummyACEVSEController.java
+++ RISE-V2G-SECC/src/main/java/org/eclipse/risev2g/secc/evseController/DummyACEVSEController.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- *  Copyright (c) 2015 Marc Mültin (Chargepartner GmbH).
+ *  Copyright (c) 2016 Dr.-Ing. Marc Mültin.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
  *  http://www.eclipse.org/legal/epl-v10.html
  *
  *  Contributors:
- *    Dr.-Ing. Marc Mültin (Chargepartner GmbH) - initial API and implementation and initial documentation
+ *    Dr.-Ing. Marc Mültin - initial API and implementation and initial documentation
  *******************************************************************************/
 package org.eclipse.risev2g.secc.evseController;
 
